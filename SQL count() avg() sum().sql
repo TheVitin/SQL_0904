05285c6aa4dc6bd3e6e539137ba3b5ac0835adf5
Vitor Hugo Alves Basso <125588333+TheVitin@users.noreply.github.com>
2024-04-09 14:45:53 -0300

    Create SQL count() avg() sum().sql

---
+++ SQL count() avg() sum().sql
@@ -0,0 +1,19 @@
+--Consulta para mostrar a quantidade de séries:
+SELECT COUNT(*) AS Quantidade_de_Series
+FROM ENTRETERIMENTO
+WHERE TIPO = 'SÉRIE';
+
+--Consulta para mostrar a quantidade de filmes:
+SELECT COUNT(*) AS Quantidade_de_Filmes
+FROM ENTRETERIMENTO
+WHERE TIPO = 'FILME';
+
+--Consulta para mostrar o tempo de duração média das séries:
+SELECT AVG(DURACAO_MINUTOS) AS Duracao_Media_Series
+FROM ENTRETERIMENTO
+WHERE TIPO = 'SÉRIE';
+
+--Consulta para somar a duração em minutos dos filmes do Brad Pitt:
+SELECT SUM(DURACAO_MINUTOS) AS Duracao_Total_Brad_Pitt_Filmes
+FROM ENTRETERIMENTO
+WHERE (ATOR_PRINCIPAL = 'Brad Pitt' OR ATRIZ_PRINCIPAL = 'Brad Pitt') AND TIPO = 'FILME';
